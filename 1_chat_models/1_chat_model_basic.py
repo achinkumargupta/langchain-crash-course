72b3225049177d22f5dbba07b44a284480ccf1f3
achinkumargupta <guptaac@outlook.com>
2026-02-07 04:15:20 +0000

    added google

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -11,17 +11,24 @@
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+import google.generativeai as genai
+
 
 # Load environment variables from .env
 load_dotenv()
 
 import os
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+for m in genai.list_models():
+    print(m.name, m.supported_generation_methods)
 for name, value in os.environ.items():
-    if name in ("OPENAI_API_KEY"):
+    if name in ("GOOGLE_API_KEY", "OPENAI_API_KEY"):
         print(name, value)
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o-mini")
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 # Invoke the model with a message
 result = model.invoke("What is 81 divided by 9?")
